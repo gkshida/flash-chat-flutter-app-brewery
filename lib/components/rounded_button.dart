648ab0f3e98892be3ff0b810e1e36e65e685f98f
Guilherme Kayo Shida <guilhermeshida@gmail.com>
2020-01-15 16:54:46 -0300

    Extracted the decoration from the input

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -22,6 +22,9 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
